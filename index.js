f5c108051f1e931eb38cfc4491fc281ffb4d5265
Suchitra Naik <susanaik@gmail.com>
2018-10-25 01:54:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ function exactMatch(drivers, list){
   return drivers.filter(function (driver) {
     let flag = false
      for (const key in list){
+<<<<<<< HEAD
           flag = driver[key] === list[key]
+=======
+          flag = driver[key] === list[key]            
+>>>>>>> 1f392e6a04a30ab05d463b3fa37674ec03928adc
         }
         return flag
     });
